12062b6258b7da098439f9301cc1f57319ee8cb3
Tom Pesman <tom@9to5.software>
2019-05-21 16:27:51 +0200

    add error response

--- lib/rocket/response.ex
+++ lib/rocket/response.ex
@@ -4,6 +4,7 @@ defmodule Rocket.Response do
   "
 
   alias HTTPoison.Response
+  alias HTTPoison.Error
 
   @success_status 200..299
   @client_error_status 400..499
@@ -22,6 +23,7 @@ defmodule Rocket.Response do
   end
 
   def parse({:ok, %Response{status_code: status, body: body}}) when status in @server_error_status, do: body
-
   def parse({_, %Response{status_code: _, body: _} = response}), do: response
+  def parse({:error, %Error{id: nil, reason: :timeout}}), do: {:error, "Timeout"}
+  def parse({:error, %Error{id: nil, reason: _reason}}), do: {:error, "Unknown error"}
 end
